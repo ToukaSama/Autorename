f43f2b0e29f2aa8bdbeef455ebd34022a99d29f5
ToukaSama <blacksummoner6659@gmail.com>
2025-05-13 17:29:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class Config(object):
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6440021089').split()]
     FORCE_SUB_CHANNELS = os.environ.get('FORCE_SUB_CHANNELS', 'codeflix_bots').split(',')
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002134572304"))
-    DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "-1002134572304"))
+    DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "-1002467460891"))
     
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
